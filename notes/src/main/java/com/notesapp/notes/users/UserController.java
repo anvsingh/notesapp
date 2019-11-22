bfb3562783792217b632bbdf54d5964729a10a64
anvsingh <anujveersingh@gmail.com>
2019-11-22 12:35:56 +0530

    User Controller

---
+++ notes/src/main/java/com/notesapp/notes/users/UserController.java
@@ -0,0 +1,39 @@
+package com.notesapp.notes.users;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired UserService service;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return service.getUsers();
+    }
+
+    @PostMapping
+    public void postUsers(@RequestBody UserDTO dto) {
+        service.add(dto);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable(required = true) long id) {
+        return service.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable(required = true) long id) {
+        service.delete(id);
+    }
+}
--- notes/src/main/java/com/notesapp/notes/users/UserRepository.java
+++ notes/src/main/java/com/notesapp/notes/users/UserRepository.java
@@ -1,11 +1,11 @@
 package com.notesapp.notes.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
