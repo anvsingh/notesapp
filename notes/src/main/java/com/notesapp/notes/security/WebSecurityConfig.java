de49ff5a372fb5406773a84686a9e17b9eea1aa5
anvsingh <anujveersingh@gmail.com>
2019-11-22 16:03:42 +0530

    Done some chanegs for testing the apis

--- notes/src/main/java/com/notesapp/notes/security/WebSecurityConfig.java
+++ notes/src/main/java/com/notesapp/notes/security/WebSecurityConfig.java
@@ -27,16 +27,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {@Bean
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 	  
+	  //http.authorizeRequests().antMatchers("/**").permitAll();
 	  
-	  http
-	  .authorizeRequests()
-      .anyRequest().authenticated()
-      .and()
-      .httpBasic();
+		/*
+		 * http .authorizeRequests().antMatchers("/**").permitAll().and()
+		 * .httpBasic().disable();
+		 */
+	  
+	  http.csrf().disable().headers().frameOptions().sameOrigin().and().
+	   authorizeRequests().anyRequest().anonymous().and().httpBasic().disable();
+	  
+	  
+		/*
+		 * http .authorizeRequests() .anyRequest().authenticated() .and() .httpBasic();
+		 */
+		/*
+		 * http .authorizeRequests() .antMatchers("/employee/me").authenticated()
+		 * .antMatchers("/**").permitAll();
+		 */
 	  
 		/*
 		 * http.authorizeRequests().anyRequest().hasAnyRole("USER_ALLOWED", "USER")
 		 * .and() .logout().permitAll().logoutSuccessUrl("/login") .and()
 		 * .csrf().disable();
+		 * 
+		 * 
 		 */
   }}
