5ec8145385184d49d5d083006277421de3ba3064
anvsingh <anujveersingh@gmail.com>
2019-11-22 16:45:22 +0530

    Fixed UserDetailsServiceImpl to pull the user from database

--- notes/src/main/java/com/notesapp/notes/security/UserDetailsServiceImpl.java
+++ notes/src/main/java/com/notesapp/notes/security/UserDetailsServiceImpl.java
@@ -1,42 +1,49 @@
 package com.notesapp.notes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.notesapp.notes.users.User;
+import com.notesapp.notes.users.UserService;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	@Autowired
+	UserService userService;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		User user = findUserbyUername(username);
 
-	    UserBuilder builder = null;
-	    if (user != null) {
-	      builder = org.springframework.security.core.userdetails.User.withUsername(username);
-	      //builder.password(new BCryptPasswordEncoder().encode(user.getPassword()));
-	      builder.password(user.getPassword());
-	      builder.roles("USER_ALLOWED");
-	    } else {
-	      throw new UsernameNotFoundException("User not found.");
-	    }
+		UserBuilder builder = null;
+		if (user != null) {
+			builder = org.springframework.security.core.userdetails.User.withUsername(username);
+			builder.password(user.getPassword());
+			// Every user has only one role i.e to see own notes so not using roles.
+			//builder.roles("USER_ALLOWED");
+		} else {
+			throw new UsernameNotFoundException("User not found.");
+		}
 
-	    return builder.build();
-		
+		return builder.build();
+
 	}
-	
-	 private User findUserbyUername(String username) {
-		    if(username.equalsIgnoreCase("test")) {
-		      //return new User(username, "admin123", "ADMIN");
-		    	User user = new User();
-		    	user.setEmail("test");
-		    	user.setPassword("{noop}test");
-		    	return user;
-		    }
-		    return null;
-		  }
+
+	private User findUserbyUername(String username) {
+		if (username != null) {
+			User user = userService.getUserById(Long.valueOf(username));
+			if(user != null) {
+				User forSecurity = new User();
+				forSecurity.setEmail(user.getEmail());
+				forSecurity.setPassword("{noop}"+user.getPassword());
+				return forSecurity;
+			}
+		}
+		return null;
+	}
 
 }
