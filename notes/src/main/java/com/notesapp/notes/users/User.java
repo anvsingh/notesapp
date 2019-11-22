822e0276ef85867faf6f31ea63846ff7ae5c27d1
anvsingh <anujveersingh@gmail.com>
2019-11-22 12:24:24 +0530

    User DTO and User Entity

---
+++ notes/src/main/java/com/notesapp/notes/users/User.java
@@ -0,0 +1,44 @@
+package com.notesapp.notes.users;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+public class User {
+	
+	private @Id @GeneratedValue Long id;
+	private String email;
+	private String password;
+	
+	private @CreatedDate LocalDateTime createdDate;
+	private @LastModifiedDate LocalDateTime lastModifiedDate;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public LocalDateTime getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+}
---
+++ notes/src/main/java/com/notesapp/notes/users/UserDTO.java
@@ -0,0 +1,44 @@
+package com.notesapp.notes.users;
+
+import java.time.LocalDateTime;
+
+public class UserDTO {
+	private Long id;
+	private String email;
+	private String password;
+	
+	private LocalDateTime createdDate;
+	private LocalDateTime lastModifiedDate;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public LocalDateTime getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+	public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+}
